6d894834de08e3c898467feef2fb5a6fb5384346
Ariel <nathan.ariel@ti.ukdw.ac.id>
2021-11-11 21:21:47 +0700

    1_D_71210776

---
+++ 1_D_71210776.py
@@ -0,0 +1,9 @@
+#menghitung luas layangan
+diagonal1 = int(input("masukkan d1:"))
+diagonal2 = int(input("masukkan d2:"))
+
+#proses
+luas = 0.5*(diagonal1*diagonal2)
+
+#print
+print("Luas Layangan", luas)
