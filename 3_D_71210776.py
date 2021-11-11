b53e92297f6a905081098a81cd19bf8d635c3e22
Ariel <nathan.ariel@ti.ukdw.ac.id>
2021-11-11 23:27:09 +0700

    3_D_71210776

---
+++ 3_D_71210776.py
@@ -0,0 +1,9 @@
+#menambah catatan belanja
+belanja1 = input("Masukkan daftar belanja 1:")
+belanja2 = input("Masukkan daftar belanja 2:")
+tambahanbelanja1 = input("Tambahkan data ke daftar belanja 1:")
+tambahanbelanja2 = input("Tambahkan data ke daftar belanja 2:")
+
+#print
+print("Daftar belanja 1 adalah", belanja1, ",", tambahanbelanja1)
+print("Daftar belanja 2 adalah", belanja2, ",", tambahanbelanja2)
