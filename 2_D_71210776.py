d177eeaa3ccbe57dc9c2c593006c8d6ebdef19fb
Ariel <nathan.ariel@ti.ukdw.ac.id>
2021-11-11 21:41:39 +0700

    2_D_71210776

---
+++ 2_D_71210776.py
@@ -0,0 +1,10 @@
+#menghitung rata-rata uang saku anak
+anakpertama = int(input("Uang saku anak pertama:"))
+anakkedua = int(input("Uang saku anak kedua:"))
+anakketiga = int(input("Uang saku anak ketiga:"))
+
+#proses
+ratarata = (anakpertama+anakkedua+anakketiga)/3
+
+#print
+print("\tRata-rata dari ", anakpertama, "+", anakkedua, "+", anakketiga, "\t adalah", ratarata)
